7047d9aeb0ba8eb898cd900ffc0326ec86b73782
Pablo Duque <menduke@live.com>
2019-09-09 20:19:23 +0200

    Added original gan training

---
+++ gan/training.py
@@ -0,0 +1,47 @@
+from keras import layers
+from keras import models
+import numpy as np
+from keras import optimizers
+from keras import losses
+
+# Assuming we have a discriminator and generator files that return model
+
+discriminator_model = discriminator.create_model(img_shape)
+discriminator_model.compile(optimizers.Adam(lr=0.00001),
+                            loss=losses.binary_crossentropy)
+
+generator_model = generator.create_model(noise_shape)
+
+z = layers.Input(shape=noise_shape)
+image = generator_model(z)
+
+discriminator_model.trainable = False
+valid = discriminator_model(image)
+
+combined_model = models.Model(z, valid)
+combined_model.compile(optimizers.Adam(lr=0.00001),
+                       loss=losses.binary_crossentropy)
+
+
+for i in range(n_iterations):
+
+    for j in range(k_steps):
+
+        idx = np.random.randint(0, images.shape[0], half_batch)
+        batch_images = images[idx]
+        noise = np.random.normal(0, 1, (half_batch, *noise_shape))
+        generated_imgs = generator_model.predict(noise)
+
+        discriminator_model.trainable = True
+        d_loss_real = discriminator_model.train_on_batch(batch_images,
+                                                        np.ones((half_batch, 1)))
+        d_loss_fake = discriminator_model.train_on_batch(generated_imgs,
+                                                        np.zeros((half_batch, 1)))
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+    noise = np.random.normal(0, 1, (batch_size, *noise_shape))
+
+    valid_y = np.array([1] * batch_size)
+    discriminator_model.trainable = False
+
+    g_loss = combined_model.train_on_batch(noise, valid_y)
